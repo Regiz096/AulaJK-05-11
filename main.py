7fd8ff0cdb7930998f3561a4a53c9423b84157d9
Ph91 <lipee.fernandes@gmail.com>
2019-11-05 20:58:43 -0300

    main sem comentario

---
+++ main.py
@@ -0,0 +1,13 @@
+a = 1 
+b = 2 
+
+soma = a +b
+
+if(soma > a):
+    print("A soma e maior que a")
+elif(soma == a):
+    print("A soma e igual a a ")
+else:
+    print("A soma e menor que a")
+        
+    
