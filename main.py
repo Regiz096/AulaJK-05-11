7356d929ca88598e9035805b06afb1febf75cd50
Ph91 <lipee.fernandes@gmail.com>
2019-11-05 21:51:21 -0300

    comentado os variaveis

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
+#nome a se escrever para realizar o exercicio
 pessoaNome = input("Nome da pessoa:")
 
+#numero para determinar o primeiro elemento da "soma"
 a = input("primeiro numero:") 
+#nuemro para determinar o segundo elemento da "soma"
 b = input("segundo numero:") 
 
 #variavel para soma de 2 numeros
@@ -14,6 +18,7 @@ elif(soma == a):# para ver se "soma" e igual a "a"
 else:# para ver se "soma" e menor que "a"
     print(pessoaNome + ",A soma e menor que a")
         
+#mostra a primeira , segunda e ultima linha do nome descrito
 print("A primeira letra do nome e " + pessoaNome[0])
 print ("A segunda letra do nome e " + pessoaNome[1])
 print("A ultima letra do nome e " + pessoaNome[4])
