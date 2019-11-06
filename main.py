fd4a552f864953493aa688160278db81d7faaadc
Ph91 <lipee.fernandes@gmail.com>
2019-11-05 21:32:43 -0300

    primeira concatenaçao

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+pessoaNome = "Regis"
+
 a = 1 
 b = 2 
 
@@ -6,10 +8,13 @@ soma = a +b
 
 #funçao para revelar se e verdadeiro ou falso
 if(soma > a): # para ver se "soma" é maior que "a"
-    print("A soma e maior que a")
+    print(pessoaNome + ",A soma e maior que a")
 elif(soma == a):# para ver se "soma" e igual a "a"
-    print("A soma e igual a a ")
+    print(pessoaNome +",A soma e igual a a ")
 else:# para ver se "soma" e menor que "a"
-    print("A soma e menor que a")
+    print(pessoaNome + ",A soma e menor que a")
         
+print("A primeira letra do nome e " + pessoaNome[0])
+print ("A segunda letra do nome e " + pessoaNome[1])
+print("A ultima letra do nome e " + pessoaNome[4])
     
