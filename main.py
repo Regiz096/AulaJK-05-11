eabe40fcae730664553431735aaf3abf6825a15e
Ph91 <lipee.fernandes@gmail.com>
2019-11-05 21:11:53 -0300

    comentado e corrigido

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ b = 2
 soma = a +b
 
 #funçao para revelar se e verdadeiro ou falso
-if(soma > a):
+if(soma > a): # para ver se "soma" é maior que "a"
     print("A soma e maior que a")
-elif(soma == a):
+elif(soma == a):# para ver se "soma" e igual a "a"
     print("A soma e igual a a ")
-else:
+else:# para ver se "soma" e menor que "a"
     print("A soma e menor que a")
         
     
